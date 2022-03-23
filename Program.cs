fb7d90b327d498a7e9181cebbdb66d915817d4cb
TifaniiDea <tifanidea@students.amikom.ac.id>
2022-03-23 20:02:59 +0700

    mengupload codingan

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            // pengesetan nilai properties
+            taxi.DriverName = "Andi";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 15;
+
+            // pemanggil method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
